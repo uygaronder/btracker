87c300ef543f1d66a52c7569e7d84b6061a44920
under4 <uonder50@gmail.com>
2022-04-05 23:07:08 +0300

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "../Login.css";
 
-import register from "../pages/login/register";
-import signin from "../pages/login/signin";
+import register from "../pages/login/Register";
+import signin from "../pages/login/SignIn";
 
 class Login extends React.Component {
     constructor(props) {
