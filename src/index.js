cee82fa7cedabac6ebfdf7d687a0c8fee1c4f64e
under4 <44830099+under4@users.noreply.github.com>
2022-04-08 18:40:10 +0300

    auth working

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "./root.css";
-import { BrowserRouter as Router, Routes, Route , Navigate} from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
 
 import Home from "./pages/Home.js";
 import Login from "./pages/Login.js";
 import Console from "./pages/Console.js";
-import "./root.css";
+import Loading from "./pages/Loading.js";
 
 const apiUrl = process.env.REACT_APP_APIURL;
-
-var authed;
-
-async function checkAuth(){
-    const promise = await fetch(`${apiUrl}/auth`,{credentials: "include"}).then((res) => res.json());
-    authed = promise.then((res) => res.err) === 0 ? true : false;
-    console.log(authed)
-
-    
-}
-checkAuth()
-
 
 ReactDOM.render(
     <React.StrictMode>
@@ -29,11 +22,45 @@ ReactDOM.render(
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login/*" element={<Login />} />
-                <Route path="/console/*" element={
-                    authed ? <Console />: <Navigate to="/login"/>
-                } />
+                <Route
+                    path="/console/*"
+                    element={
+                        <ProtectedRoute>
+                            <Console />
+                        </ProtectedRoute>
+                    }
+                />
             </Routes>
         </Router>
     </React.StrictMode>,
     document.getElementById("root")
 );
+
+function ProtectedRoute({ children }) {
+    const [isAuthenticated, setAuth] = useState(null);
+    const [fetched, setFetch] = useState(false);
+    useEffect(() => {
+        const auth = async () => {
+            try {
+                const promise = await fetch(`${apiUrl}/auth`, {
+                    credentials: "include",
+                }).then((res) => res.json());
+                setAuth(promise.err === 0 ? true : false);
+                setFetch(true);
+            } catch (e) {
+                return e;
+            }
+        };
+
+        auth();
+    }, []);
+    return fetched ? (
+        isAuthenticated ? (
+            <Console />
+        ) : (
+            <Navigate to={"/login"} />
+        )
+    ) : (
+        <Loading />
+    );
+}
---
+++ src/pages/Loading.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../Loading.css";
+
+const Loading = () => {
+    return <div id="loading"></div>;
+};
+
+export default Loading;
