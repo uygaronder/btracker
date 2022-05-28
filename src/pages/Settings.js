b2cc0707770a6faf31be1e463b8fd14cf6a368b3
under4 <44830099+under4@users.noreply.github.com>
2022-05-28 15:15:42 +0300

    settings stuff

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -3,6 +3,7 @@ import React from "react";
 import "../css/Settings.css";
 
 var apiUrl = process.env.REACT_APP_APIURL;
+var appURL = process.env.REACT_APP_APPURL;
 class Settings extends React.Component {
     constructor(props) {
         super(props);
@@ -36,10 +37,16 @@ class Settings extends React.Component {
                     user: this.props.consoleState.usrId,
                     data: this.state.avatar,
                 }),
-            });
+            }).then(
+                () => (window.location.href = `${appURL}/console/settings`)
+            );
         } catch (error) {
             console.error(error);
         }
+    }
+
+    labelColorChanged(e) {
+        console.log(e.target.value);
     }
 
     render() {
@@ -85,6 +92,41 @@ class Settings extends React.Component {
                     <div className="settingsContainer">
                         <div className="setting">
                             <h4>Change Label Colors</h4>
+                            <div id="labels">
+                                {Object.keys(
+                                    this.props.consoleState.team.labels
+                                ).map((key) => {
+                                    const color =
+                                        this.props.consoleState.team.labels[
+                                            key
+                                        ];
+                                    return (
+                                        <div>
+                                            <label
+                                                style={{
+                                                    backgroundColor: color,
+                                                }}
+                                                className="label"
+                                                htmlFor={`change${key}`}
+                                            >
+                                                {key}
+                                            </label>
+                                            <input
+                                                type="color"
+                                                id={`change${key}`}
+                                                defaultValue={color}
+                                                onChange={(e) =>
+                                                    this.labelColorChanged(e)
+                                                }
+                                            />
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                            <div className="buttons">
+                                <button>Save Changes</button>
+                                <button>Reset Changes</button>
+                            </div>
                         </div>
                     </div>
                 </div>
