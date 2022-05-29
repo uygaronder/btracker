b98d803475dc47327c4e15d52abb6902db2bd4df
under4 <uonder50@gmail.com>
2022-05-29 21:08:35 +0300

    home stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,13 @@ import bugSvg from "../res/svg/bugPic.svg";
 
 import { Link } from "react-router-dom";
 import logo from "../res/anteater.png";
+import appPreview from "../res/app-preview.png";
+
 import chevron from "../res/svg/chevron-up.svg";
+import bug from "../res/svg/bug.svg";
+import team from "../res/svg/team.svg";
+import clock from "../res/svg/clock.svg";
+import label from "../res/svg/label.svg";
 class Home extends React.Component {
     componentDidMount() {
         document.title = "Bugsnack";
@@ -20,7 +26,7 @@ class Home extends React.Component {
                     </Link>
                     <ul id="anchors">
                         <li>
-                            <a href="*">Features</a>
+                            <a href="#features">Features</a>
                         </li>
                         <li>
                             <a
@@ -31,7 +37,7 @@ class Home extends React.Component {
                             </a>
                         </li>
                         <li>
-                            <a href="*">Resources</a>
+                            <a href="*">Contact Us</a>
                         </li>
                     </ul>
                     <ul id="navButtons">
@@ -47,7 +53,7 @@ class Home extends React.Component {
                 </nav>
                 {/* Hero Section */}
                 <section id="hero">
-                    <div>
+                    <div id="heroCTA">
                         <h2>Ship great products with Bugsnack</h2>
                         <p>
                             Bugsnack is an open source bug tracking tool
@@ -60,8 +66,8 @@ class Home extends React.Component {
                             </Link>
                         </div>
                     </div>
-                    <div>
-                        <img src={bugSvg} alt="svg" class="animated" />
+                    <div id="heroImage">
+                        <img src={appPreview} alt="svg" class="animated" />
                     </div>
                     <a href="#features">
                         <img id="chevron" src={chevron} alt="" />
@@ -78,6 +84,7 @@ class Home extends React.Component {
                     </div>
                     <div class="featuresContainer">
                         <div>
+                            <img src={bug} alt="" />
                             <div>
                                 <h3>Bug/Issue Management</h3>
                                 <p>
@@ -89,6 +96,7 @@ class Home extends React.Component {
                             </div>
                         </div>
                         <div>
+                            <img src={team} alt="" />
                             <div>
                                 <h3>Teams</h3>
                                 <p>
@@ -99,6 +107,7 @@ class Home extends React.Component {
                             </div>
                         </div>
                         <div>
+                            <img src={clock} alt="" />
                             <div>
                                 <h3>Time Tracking</h3>
                                 <p>
@@ -107,7 +116,23 @@ class Home extends React.Component {
                                 </p>
                             </div>
                         </div>
+                        <div>
+                            <img src={label} alt="" />
+                            <div>
+                                <h3>Labeling</h3>
+                                <p>
+                                    Create custom labels for your bugs so you
+                                    can find the exact bug you are looking for
+                                </p>
+                            </div>
+                        </div>
                     </div>
+                </section>
+                <section id="footerCTA">
+                    <h2>Try Bugsnack for free</h2>
+                    <Link to="/login/register">
+                        <button>Get Started</button>
+                    </Link>
                 </section>
             </div>
         );
